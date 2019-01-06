ac73fa36737c53940a154deccb63fbac788bd893
Griffin Austin <gaustin25@gmail.com>
2019-01-06 05:27:43 -0700

    Add wood and stone resources and respective buildings

--- main.js
+++ main.js
@@ -1,36 +1,79 @@
-// Resources
-var food = 0;
+const COST = 10;
 
-// Buildings
-var farms = 0;
+var resources = {
+  "food": 0,
+  "wood": 0,
+  "stone": 0
+}
+
+var buildings = {
+  "farms": 0,
+  "lumberjackHuts": 0,
+  "mines": 0
+}
 
 function addFood(x) {
-  food += x;
-  document.getElementById("food").innerHTML = "Food: " + food;
+  resources.food += x;
+  document.getElementById("food").innerHTML = "Food: " + resources.food;
+}
+
+function addWood(x) {
+  resources.wood += x;
+  document.getElementById("wood").innerHTML = "Wood: " + resources.wood;
+}
+
+function addStone(x) {
+  resources.stone += x;
+  document.getElementById("stone").innerHTML = "Stone: " + resources.stone;
 }
 
 function buildFarm() {
-  var farmCost = Math.floor(10 * Math.pow(1.1, farms));
-  if(food >= farmCost){
-    farms++;
-    food -= farmCost;
-    document.getElementById("farms").innerHTML = farms;
-    document.getElementById("food").innerHTML = "Food: " + food;
-    document.getElementById("foodPerSec").innerHTML = " (" + farms + " per second)";
+  var farmCost = Math.floor(COST * Math.pow(1.1, buildings.farms));
+  if(resources.food >= farmCost){
+    buildings.farms++;
+    resources.food -= farmCost;
+    document.getElementById("farms").innerHTML = buildings.farms;
+    document.getElementById("food").innerHTML = "Food: " + resources.food;
   }
-  var nextCost = Math.floor(10 * Math.pow(1.1, farms));
+  var nextCost = Math.floor(COST * Math.pow(1.1, buildings.farms));
   document.getElementById("farmCost").innerHTML = nextCost;
+}
+
+function buildLumberjackHut() {
+  var lumberjackHutCost = Math.floor(COST * Math.pow(1.1, buildings.lumberjackHuts));
+  if(resources.wood >= lumberjackHutCost){
+    buildings.lumberjackHuts++;
+    resources.wood -= lumberjackHutCost;
+    document.getElementById("lumberjackHuts").innerHTML = buildings.lumberjackHuts;
+    document.getElementById("wood").innerHTML = "Wood: " + resources.wood;
+  }
+  var nextCost = Math.floor(COST * Math.pow(1.1, buildings.lumberjackHuts));
+  document.getElementById("lumberjackHutCost").innerHTML = nextCost;
+}
+
+function buildMine() {
+  var mineCost = Math.floor(COST * Math.pow(1.1, buildings.mines));
+  if(resources.stone >= mineCost){
+    buildings.mines++;
+    resources.stone -= mineCost;
+    document.getElementById("mines").innerHTML = buildings.mines;
+    document.getElementById("stone").innerHTML = "Stone: " + resources.stone;
+  }
+  var nextCost = Math.floor(COST * Math.pow(1.1, buildings.mines));
+  document.getElementById("mineCost").innerHTML = nextCost;
 }
 
 // Game loop (1000ms per loop)
 window.setInterval(function() {
-  addFood(farms);
+  addFood(buildings.farms);
+  addWood(buildings.lumberjackHuts);
+  addStone(buildings.mines);
 }, 1000);
 
 function save() {
   var save = {
-    food: food,
-    farms: farms
+    resources: resources,
+    buildings: buildings
   }
   localStorage.setItem("save", JSON.stringify(save));
   console.log("Game saved.");
@@ -38,12 +81,12 @@ function save() {
 
 function load() {
   var savegame = JSON.parse(localStorage.getItem("save"));
-  if (typeof savegame.food !== "undefined") food = savegame.food;
-  if (typeof savegame.farms !== "undefined") farms = savegame.farms;
+  if (typeof savegame.resources !== "undefined") resources = savegame.resources;
+  if (typeof savegame.buildings !== "undefined") buildings = savegame.buildings;
   console.log("Game loaded.");
 }
 
 function deleteSave() {
   localStorage.removeItem("save");
-  console.log("Save deleted.");
+  console.log("Game deleted.");
 }
